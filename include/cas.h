622bbb6ff16be13a66a3d767a69b8757e919d83f
Carlo Di Giuseppe <mi0772@gmail.com>
2025-07-17 14:55:16 +0200

    cache fs

---
+++ include/cas.h
@@ -0,0 +1,11 @@
+/**
+ * Project: PodCache
+ * Author: Carlo Di Giuseppe
+ * Date: 17/07/25
+ * License: MIT
+ */
+ 
+#ifndef CAS_H
+#define CAS_H
+
+#endif //CAS_H
---
+++ src/cas.c
@@ -0,0 +1,8 @@
+/**
+ * Project: PodCache
+ * Author: Carlo Di Giuseppe
+ * Date: 17/07/25
+ * License: MIT
+ */
+
+#include "cas.h"
